6f4dfc283652752920aa75d8cb535272ae832545
Sebastian <wangjiaxin6667@gmail.com>
2022-01-11 19:55:40 +0800

    Adding video upload

--- src/views/course/components/Video/Form.vue
+++ src/views/course/components/Video/Form.vue
@@ -14,6 +14,28 @@
           <el-radio :label="false">Default</el-radio>
         </el-radio-group>
       </el-form-item>
+      <!-- 上传视频 -->
+      <el-form-item label="Video upload">
+        <el-upload
+          ref="upload"
+          :auto-upload="false"
+          :on-success="handleUploadSuccess"
+          :on-error="handleUploadError"
+          :on-exceed="handleUploadExceed"
+          :file-list="fileList"
+          :limit="1"
+          action="http://127.0.0.1:8130/vod/media/upload"
+        >
+          <el-button slot="trigger" size="small" type="primary">Choose video</el-button>
+          <el-button
+            :disabled="uploadBtnDisabled"
+            style="margin-left: 10px;"
+            size="small"
+            type="success"
+            @click="submitUpload()"
+          >上传</el-button>
+        </el-upload>
+      </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
       <el-button @click="close()">Cancel</el-button>
@@ -32,7 +54,9 @@ export default {
       video: {
         sort: 0,
         free: false
-      }
+      },
+      fileList: [], // 上传文件列表
+      uploadBtnDisabled: false
     }
   },
 
@@ -43,6 +67,10 @@ export default {
       if (videoId) {
         videoApi.getById(videoId).then(response => {
           this.video = response.data.item
+          // 回显
+          if (this.video.videoOriginalName) {
+            this.fileList = [{ 'name': this.video.videoOriginalName }]
+          }
         })
       }
     },
@@ -58,6 +86,7 @@ export default {
         sort: 0,
         free: false
       }
+      this.fileList = [] // 重置视频上传列表
     },
 
     saveOrUpdate() {
@@ -87,6 +116,30 @@ export default {
         // 刷新列表
         this.$parent.fetchNodeList()
       })
+    },
+    handleUploadExceed(files, fileList) {
+      this.$message.warning('If you want to re-upload the video, please delete the uploaded video first')
+    },
+    // 上传
+    submitUpload() {
+      this.uploadBtnDisabled = true
+      this.$refs.upload.submit() // 提交上传请求
+    },
+    // 视频上传成功的回调
+    handleUploadSuccess(response, file, fileList) {
+      this.uploadBtnDisabled = false
+      if (response.success) {
+        this.video.videoSourceId = response.data.videoId
+        this.video.videoOriginalName = file.name
+      } else {
+        this.$message.error('UploadFailed1')
+      }
+    },
+
+    // 失败回调
+    handleUploadError() {
+      this.uploadBtnDisabled = false
+      this.$message.error('UploadFailed2')
     }
   }
 }
